f35a07335a83f369266b16fe2655414460895078
Love <lovesoni607@gmail.com>
2023-04-23 14:44:28 +0530

    get call with query params

--- src/test/java/apis/GetApi.java
+++ src/test/java/apis/GetApi.java
@@ -3,6 +3,7 @@ package apis;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.RequestOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -25,17 +26,29 @@ public class GetApi {
         System.out.println(statusResText);
 
         byte[] bodyArray = apiResponse.body();
-        for(byte b : bodyArray){
-            System.out.print((char)b);
+        for (byte b : bodyArray) {
+            System.out.print((char) b);
         }
         ObjectMapper objectMapper = new ObjectMapper();
         JsonNode jsonNode = objectMapper.readTree(bodyArray);
         System.out.println(jsonNode.toPrettyString());
 
         String url = apiResponse.url();
-        System.out.println("Api url is : "+url);
+        System.out.println("Api url is : " + url);
 
-        Map<String,String> responseHeaders = apiResponse.headers();
-        System.out.println("Headers are : "+ responseHeaders);
+        Map<String, String> responseHeaders = apiResponse.headers();
+        System.out.println("Headers are : " + responseHeaders);
+    }
+
+    @Test
+    public void getCallWithQueryParams() {
+        Playwright playwright = Playwright.create();
+        APIRequest request = playwright.request();
+        APIRequestContext apiRequestContext = request.newContext();
+        RequestOptions requestOptions = RequestOptions.create();
+        requestOptions.setQueryParam("gender","malet stat");
+        APIResponse apiResponse = apiRequestContext.get("https://gorest.co.in/public/v2/users", requestOptions);
+        int statusCode = apiResponse.status();
+        System.out.println(apiResponse.text());
     }
 }
